051efaf6e7866927cefa3cfc282fa1423cce7c0d
Jessica Green <41573865+JessicaGreen@users.noreply.github.com>
2020-08-04 17:13:04 +0100

    Update siegfried_droid_analysis.sql

--- siegfried_droid_analysis.sql
+++ siegfried_droid_analysis.sql
@@ -1,9 +1,10 @@
 ---Analyze Siegfried/DROID report by grouping technical metadata about each of the files (tif, xml, etc.) within a folder 
 ---Image files (tiff, jpg) represent one  digitized page of an item, each folder contains one or more files that represent one physical item
 ---The Siegfried/DROID report contains one row per file, but most other data sources have one row per folder/item
----Save this output as new csv file - relevant columns can then be joined and analyzed with folder-level analysis spreadsheets
+---Save this output as new csv file or as a view in DB Browser to query and join with folder-level analysis spreadsheets
 WITH files_and_paths AS (
-	SELECT FILE_PATH, substr(FILE_PATH, 1, length(FILE_PATH) - length(NAME) - 1) AS FOLDER_PATH, --substr() removes the file name and trailing \ from the file path by calculating: (length of file name) - (length of file path) - 1 
+	SELECT FILE_PATH, 
+	substr(FILE_PATH, 1, length(FILE_PATH) - length(NAME) - 1) AS FOLDER_PATH, --substr() removes the file name and trailing \ from the file path by calculating: (length of file name) - (length of file path) - 1 
 	NAME, 
 	CASE WHEN upper(EXT) = 'TIF' OR upper(EXT) = 'TIFF' THEN NAME --these CASE WHEN statements limits results to only files with matching file extensions or file names and creates new filtered columns to use below - in this case XML_NAMES
 	ELSE NULL END TIF_NAME,
@@ -11,7 +12,7 @@ WITH files_and_paths AS (
 	ELSE NULL END CONTROL_NAME,
 	CASE WHEN upper(EXT) = 'XML' THEN NAME --upper() makes it possible to do a case insensitive match for 'tif' or 'TIFF' - it normalising the data in the extension column by making it all uppercase, then has to check for only uppercase matches
 	ELSE NULL END XML_NAME,
-	coalesce(SIZE, 0) AS SIZE, --coalesce() replaces any null value in the size column with '0' to make calculations possible
+	coalesce(SIZE, 0) AS SIZE, --coalesce() replaces any null value in the size column with '0' - makes calculations possible for folders with files with errors/no file size
 	CASE WHEN upper(EXT) = 'TIF' OR upper(EXT) = 'TIFF' THEN coalesce(SIZE, 0)
 	ELSE 0 END TIF_SIZE,
 	CASE WHEN upper(EXT) = 'XML' THEN coalesce(SIZE, 0)
@@ -20,12 +21,12 @@ WITH files_and_paths AS (
 	ELSE NULL END SMALL_TIF_NAME,
 	EXT, EXTENSION_MISMATCH, 
 	CASE WHEN STATUS <> 'Done' THEN STATUS ELSE NULL END ERRORS --renames STATUS column ERRORS and limits values to only those with errors
-	FROM source_AAC_Siegfried_201908
+	FROM amemm_siegfried_202006) --name of table to analyse
 	--WHERE EXT = 'xml' OR EXT = 'tif' --limits the results of the whole query by only testing on a subset of the data - helpful to test results of new queries
-	--WHERE STATUS <> 'Done' --limits results by only folders that have files with errors
-	LIMIT 100000) 
-SELECT FOLDER_PATH AS "Absolute folder path",
-SUM(SIZE)/1024/1024 AS "Total file size (MB)", --calculates total folder size by summing SIZE of all files, divides the SIZE by 1024 twice to convert B to MB
+	--WHERE STATUS <> 'Done' --limits results by only folders that have files with errors - helpful for 'error report'
+	--LIMIT 100000) 
+SELECT FOLDER_PATH AS "Folder path",
+ROUND(SUM(SIZE)/1024/1024) AS "Total file size (MB)", --calculates total folder size by summing SIZE of all files, divides the SIZE by 1024 twice to convert B to MB
 SUM(SIZE) AS "Total file size (B)", 
 SUM(TIF_SIZE + XML_SIZE) AS "Size of tif and xml files (B)",
 SUM(TIF_SIZE) AS "Size of tif files (B)",
@@ -36,8 +37,8 @@ COUNT(TIF_NAME) AS "Number of tif files", --only calculates number of files with
 COUNT(TIF_NAME - CONTROL_NAME) AS "Number of tif files (excl. control shots)", 
 COUNT(XML_NAME) AS "Number of xml files",
 replace(group_concat(DISTINCT EXT), ',', '; ') AS  "File extensions", --concatenates all file extensions in folder and replaces default comma seperators with a semi-colon and space
-(SELECT group_concat(NAME, '; ') WHERE EXTENSION_MISMATCH = 'TRUE') AS "File extension mismatches", --concatenates all file names in folder where there is an extension mismatch - file type doesn't match file extension
+(SELECT group_concat(NAME, '; ') WHERE upper(EXTENSION_MISMATCH) = 'TRUE') AS "File extension mismatches", --concatenates all file names in folder where there is an extension mismatch - file type doesn't match file extension
 replace(group_concat(DISTINCT ERRORS), ',', '; ') AS "Errors",
 replace(group_concat(DISTINCT SMALL_TIF_NAME), ',', '; ') AS "Tif files under 20 KB"
 FROM files_and_paths
-GROUP BY "Absolute folder path";
+GROUP BY "Folder path";
